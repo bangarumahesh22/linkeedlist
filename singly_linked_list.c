3d8b322e7338ec69251c2508e8519bf72ae42230
msnrk2006 <msnrk2006@gmail.com>
2025-11-03 08:35:44 +0530

    Added singly linked list implementation in C

---
+++ singly_linked_list.c
@@ -0,0 +1,52 @@
+
+
+// Singly Linked List implementation in C
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* head = NULL;
+
+// Function to insert at the end
+void insertAtEnd(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    struct Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+// Function to display the list
+void display() {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    insertAtEnd(10);
+    insertAtEnd(20);
+    insertAtEnd(30);
+
+    printf("Singly Linked List: ");
+    display();
+
+    return 0;
+}
